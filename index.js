722bd8204c3894be1a901b3fe58798057589df02
GOB Tech <88345156+gobstech@users.noreply.github.com>
2025-02-26 16:29:10 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
-import './Botao.css';
+import './CampoTexto.css';
+import React, { useState } from 'react';
 
-const Botao = (props) => {
-  return <button className="botao-criar-card">{props.children}</button>;
+export const CampoTexto = (props) => {
+  const labelModificado = `${props.label}`;
+  const placeholderModificada = `${props.placeholder}...`;
+
+  const aoDigitar = (event) => {
+    props.aoAlterado(event.target.value);
+  };
+
+  return (
+    <div className="campo-texto">
+      <label>{labelModificado}</label>
+      <input
+        value={props.valor}
+        onChange={aoDigitar}
+        required={props.obrigatorio}
+        placeholder={placeholderModificada}
+      />
+    </div>
+  );
 };
 
-export default Botao;
+export default CampoTexto;
