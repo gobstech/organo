19c020474a64e33c0d5f63a6bdcdc9326f4f9653
GOB Tech <88345156+gobstech@users.noreply.github.com>
2025-02-26 16:25:04 -0300

    Add files via upload

---
+++ Banner.js
@@ -0,0 +1,12 @@
+import './Banner.css';
+
+const Banner = () => {
+  return (
+    //JSX
+    <header className="banner">
+      <img src="/imagens/imagens/banner.png" alt="Banner principal do Organo" />
+    </header>
+  );
+};
+
+export default Banner;
